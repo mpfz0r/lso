2e66cdb3bf400d600834259a22160e2f849082ba
Marco Pfatschbacher <marco.pfatschbacher@narrowin.ch>
2026-03-06 22:23:06 +0100

    Reduce to ansible-core

--- test/routes/test_playbook.py
+++ test/routes/test_playbook.py
@@ -37,8 +37,8 @@ def test_playbook_endpoint_dict_inventory_success(
     params = {
         "playbook_name": "placeholder.yaml",
         "inventory": {
-            "_meta": {"vars": {"host1.local": {"foo": "bar"}, "host2.local": {"hello": "world"}}},
-            "all": {"hosts": {"host1.local": None, "host2.local": None}},
+            "_meta": {"vars": {"host1_local": {"foo": "bar"}, "host2_local": {"hello": "world"}}},
+            "all": {"hosts": {"host1_local": None, "host2_local": None}},
         },
         "extra_vars": {"dry_run": True, "commit_comment": "I am a robot!"},
     }
@@ -52,7 +52,7 @@ def test_playbook_endpoint_dict_inventory_success(
 
     with patch("lso.routes.playbook.ansible_runner.run", new=mocked_ansible_runner_run):
         rv = client.post("/api/playbook/", json=params)
-        assert rv.status_code == status.HTTP_201_CREATED
+        assert rv.status_code == status.HTTP_201_CREATED, rv.text
         response = rv.json()
 
     assert isinstance(response, dict)
@@ -96,8 +96,7 @@ def test_playbook_endpoint_invalid_host_vars(client: TestClient, mocked_ansible_
 
     assert isinstance(response, dict)
     assert response["detail"] == [
-        '[WARNING]: Skipping unexpected key (host_vars) in group (_meta), only "vars",\n',
-        '"children" and "hosts" are valid\n',
+        '[WARNING]: Skipping unexpected key (host_vars) in group (_meta), only "vars", "children" and "hosts" are valid\n',
     ]
     responses.assert_call_count(TEST_CALLBACK_URL, 0)
 
